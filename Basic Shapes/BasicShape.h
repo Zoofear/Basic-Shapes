700587b82e9de3c87921504122e7e575084b6a76
Zoofear <misterz0312@gmail.com>
2024-12-12 15:16:40 -0500

    Add project files.

---
+++ Basic Shapes/BasicShape.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class BasicShape
+{
+public:
+
+	BasicShape(double a = 1, string n = "name") : area(a), name(n) {}
+
+	void setArea(double a)
+	{
+		area = a;
+	}
+	
+	void setName(string n)
+	{
+		name = n;
+	}
+
+	double returnArea()
+	{
+		return area;
+	}
+	
+	string returnName()
+	{
+		return name;
+	}
+
+	virtual double calcArea();
+
+private:
+
+	double area;
+
+	string name;
+
+};
+
---
+++ Basic Shapes/Circle.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include "BasicShape.h"
+
+using namespace std;
+
+
+class Circle : public BasicShape
+{
+public:
+
+
+
+
+private:
+
+	double xCenter;
+
+	double yCenter;
+
+	double radius;
+
+};
+
